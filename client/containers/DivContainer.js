1c2a079efef2c0d6bad08b5e7ae047fcb856948c
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-11-13 11:07:10 +0000

    Branch for integrating gauge Component

--- client/containers/DivContainer.js
+++ client/containers/DivContainer.js
@@ -1,8 +1,4 @@
-
-
-/*eslint-disable no-unused-vars*/
 import React from 'react';
-/*eslint-disable no-unused-vars*/
 import DivComponent from '../components/DivComponent.js';
 import {SuperContainer} from './SuperContainer.js';
 
---
+++ client/containers/GaugeContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import GaugeComponent from '../components/GaugeComponent.js';
+import {SuperContainer} from './SuperContainer.js'
+
+export class GaugeContainer extends SuperContainer {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        /*I think we use state here, inherited from the SuperContainer,
+        ie not the react component inheritence that would normally use
+        props */
+        return(
+            <GaugeComponent EPICSValue={this.state.EPICSValue} />
+        );
+    }
+}
