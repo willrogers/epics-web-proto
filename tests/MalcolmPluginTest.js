b84b818e5132e5038190a7561c82da16afef7c1f
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2017-11-24 12:48:10 +0000

    Test file created - working towards tests

--- tests/MalcolmPluginTest.js
+++ tests/MalcolmPluginTest.js
@@ -1,2 +1,27 @@
-import MalcolmConnection from './../client/connection/MalcolmPlugin.js';
+import {MalcolmConnection} from './../client/connection/MalcolmPlugin.js';
+import {expect} from 'chai';
+import sinon from 'sinon';
+import enzyme from 'enzyme';
 
+
+const WebSocketURL = 'ws://pc0088:8080/ws';
+
+describe('MalcolmPlugin', function() {
+
+    const spyCallback = sinon.spy();
+    const testPlugin = new MalcolmConnection(spyCallback, WebSocketURL);
+
+    it('have a constructor', function(){
+        expect(testPlugin.instance().constructor()).exists(true);
+    });
+
+
+
+    it('Should call the callback passed to it with data from Malcolm', function() {
+        //Not sure how to 'fake' a message from malcolm, we have come across this
+        //problem before and decided not to tackle it.
+        //The only thing I can think of is to somehow mock the 'onmessage' call from
+        //a websocket - potentially using something like project-react's fake submission.
+    });
+
+});
