982d61eae2de86a4ffa167b15168e86a0b115996
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2018-01-19 16:39:14 +0000

    ESlint fixes

--- client/redux/EPICSReducer.js
+++ client/redux/EPICSReducer.js
@@ -19,31 +19,31 @@ function EPICSReducer(state = initialState, action) {
     // properties over, allowing us to avoid mutating the state.
     switch(action.type) {
 
-        //Update the state of the PVs that we are listening to.
-        //Create a copy of the current epicsData. Then target the
-        //desired PV (pvName) in the old data and update it (pvValue).
-        //Apply this to our state.
-        case UPDATE_PV: {
-            const newEpicsData = Object.assign({}, state.epicsData);
-            newEpicsData[action.payload.pvName] = action.payload.pvValue;
-            return Object.assign({}, state, {
-                epicsData: newEpicsData
-            });
-        }
+    //Update the state of the PVs that we are listening to.
+    //Create a copy of the current epicsData. Then target the
+    //desired PV (pvName) in the old data and update it (pvValue).
+    //Apply this to our state.
+    case UPDATE_PV: {
+        const newEpicsData = Object.assign({}, state.epicsData);
+        newEpicsData[action.payload.pvName] = action.payload.pvValue;
+        return Object.assign({}, state, {
+            epicsData: newEpicsData
+        });
+    }
 
-        //Update the state of the websocket status. Make a copy of the
-        //state that reflects the wsStatus, and use it to replace the
-        //old state.
-        case UPDATE_WS_READYSTATE: {
-            return Object.assign({}, state, {
-                wsReadyState: action.payload.wsStatus
-            });
-        }
+    //Update the state of the websocket status. Make a copy of the
+    //state that reflects the wsStatus, and use it to replace the
+    //old state.
+    case UPDATE_WS_READYSTATE: {
+        return Object.assign({}, state, {
+            wsReadyState: action.payload.wsStatus
+        });
+    }
 
-        //If nothing matches, return the default state.
-        default: {
-            return state;
-        }
+    //If nothing matches, return the default state.
+    default: {
+        return state;
+    }
 
     }
 }
--- client/redux/WebsockMiddleware.js
+++ client/redux/WebsockMiddleware.js
@@ -21,52 +21,52 @@ const websockMiddleware = store => next => action => {
     //Check the type of the action
     switch (action.type) {
 
-        //If no connObj exists, create it using the URL provided in the
-        //action.
-        case CREATE_CONNECTION: {
-            if (connectionObject === null) {
-                connectionObject = new ServerInterface(action.payload.webSocketURL)
-            }
-            break;
+    //If no connObj exists, create it using the URL provided in the
+    //action.
+    case CREATE_CONNECTION: {
+        if (connectionObject === null) {
+            connectionObject = new ServerInterface(action.payload.webSocketURL);
         }
+        break;
+    }
 
-        //Provided there is a connObj, call the monitorPV method of the
-        //connObj and create a subscription to listen to a PV
-        case SUBSCRIBE_TO_PV: {
-            if (connectionObject !== null) {
-                connectionObject.monitorPV(
-                    action.payload.id,
-                    action.payload.block,
-                    action.payload.property);
-            }
-            break;
+    //Provided there is a connObj, call the monitorPV method of the
+    //connObj and create a subscription to listen to a PV
+    case SUBSCRIBE_TO_PV: {
+        if (connectionObject !== null) {
+            connectionObject.monitorPV(
+                action.payload.id,
+                action.payload.block,
+                action.payload.property);
         }
+        break;
+    }
 
-        //Provided there is a connObj, destroy the subscription identified
-        // by the supplied ID
-        case UNSUBSCRIBE_TO_PV: {
-            if (connectionObject !== null) {
-                connectionObject.destroyMonitor(action.payload.unsubID);
-            }
-            break;
+    //Provided there is a connObj, destroy the subscription identified
+    // by the supplied ID
+    case UNSUBSCRIBE_TO_PV: {
+        if (connectionObject !== null) {
+            connectionObject.destroyMonitor(action.payload.unsubID);
         }
+        break;
+    }
 
-        //To close the websocket we first need to kill all of the
-        //subscriptions.
-        case CLOSE_WEBSOCKET: {
-            if (connectionObject !== null) {
-                connectionObject.destroyAllMonitors();
-                connectionObject.closeWebsocket();
-            }
+    //To close the websocket we first need to kill all of the
+    //subscriptions.
+    case CLOSE_WEBSOCKET: {
+        if (connectionObject !== null) {
+            connectionObject.destroyAllMonitors();
+            connectionObject.closeWebsocket();
         }
+    }
         break;
 
         //If the action type doesn't match any of these cases, forward it
         //to the next link the chain - currently this is our reducer.
-        default: {
-            next(action)
-        }
+    default: {
+        next(action);
+    }
     }
 };
 
-export default websockMiddleware
+export default websockMiddleware;
