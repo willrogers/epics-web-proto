f5c1533702c01d52aad36626e1016d34026ac2e2
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2018-01-19 14:24:50 +0000

    Comments updates

--- client/redux/EPICSReducer.js
+++ client/redux/EPICSReducer.js
@@ -1,20 +1,13 @@
 import {
     UPDATE_PV,
-    // CREATE_CONNECTION,
-    // SUBSCRIBE_TO_PV,
-    // UNSUBSCRIBE_TO_PV,
     UPDATE_WS_READYSTATE,
-    CLOSE_WEBSOCKET
 } from '../client/actions/EPICSActions.js';
 
 //Initial state of our store
 const initialState = {
     epicsData: {},
-    connectionObject: null,
     wsReadyState: null
 };
-
-
 
 //Default params initialises state when nothing is passed
 function EPICSReducer(state = initialState, action) {
@@ -25,23 +18,6 @@ function EPICSReducer(state = initialState, action) {
     // properties over, allowing us to avoid mutating the state.
     switch(action.type) {
 
-    // case SUBSCRIBE_TO_PV: {
-    //     if (state.connectionObject !== null) {
-    //         state.connectionObject.monitorPV(
-    //             action.payload.id,
-    //             action.payload.block,
-    //             action.payload.property);
-    //     }
-    //     return state;
-    // }
-
-    // case UNSUBSCRIBE_TO_PV: {
-    //     if (state.connectionObject !== null) {
-    //         state.connectionObject.destroyMonitor(action.payload.unsubID);
-    //     }
-    //     return state;
-    // }
-
     case UPDATE_PV: {
         const newEpicsData = Object.assign({}, state.epicsData);
         newEpicsData[action.payload.pvName] = action.payload.pvValue;
@@ -50,25 +26,10 @@ function EPICSReducer(state = initialState, action) {
         });
     }
 
-    // case CREATE_CONNECTION: {
-    //     if (state.connectionObject === null) {
-    //         return Object.assign({}, state, {
-    //             connectionObject: new ServerInterface(action.payload.webSocketURL)
-    //         });
-    //     }
-    //     return state;
-    // }
-
     case UPDATE_WS_READYSTATE: {
         return Object.assign({}, state, {
             wsReadyState: action.payload.wsStatus
         });
-    }
-
-    case CLOSE_WEBSOCKET: {
-        state.connectionObject.destroyAllMonitors();
-        state.connectionObject.closeWebsocket();
-        return state;
     }
 
     //If nothing matches, return the default state.
--- client/redux/WebsockMiddleware.js
+++ client/redux/WebsockMiddleware.js
@@ -1,27 +1,18 @@
-//Import the connection-y stuff
-//We will keep the websocket in here, If we pick out a websocket-y
-//action then we will handle the logic in here, and pass the return
-//back to action creator, to send to the store. If we don't recognise
-//the actionType here (in this case, we will be looking at the return from
-// an action that has previously gone through this middleware.)
-// Then we will pass it on to the standard reducer fr
 
 import {
     CREATE_CONNECTION,
     SUBSCRIBE_TO_PV,
     UNSUBSCRIBE_TO_PV,
-    UPDATE_WS_READYSTATE,
     CLOSE_WEBSOCKET
 } from '../client/actions/EPICSActions.js';
 
 import {ServerInterface} from '../client/connection/ServerInterface.js';
 
-let connectionObject = null
+let connectionObject = null;
 
-const websockMiddleware = store => next => action => {
+const websockMiddleware = next => action => {
 
     switch (action.type) {
-
         case CREATE_CONNECTION: {
             if (connectionObject === null) {
                 connectionObject = new ServerInterface(action.payload.webSocketURL)
@@ -49,8 +40,8 @@ const websockMiddleware = store => next => action => {
         case CLOSE_WEBSOCKET: {
             connectionObject.destroyAllMonitors();
             connectionObject.closeWebsocket();
-            return state;
         }
+        break;
 
         default: {
             next(action)
@@ -59,9 +50,3 @@ const websockMiddleware = store => next => action => {
 };
 
 export default websockMiddleware
-
-//
-// if (action.type === CLOSE_WEBSOCKET) {
-//     console.log("Reached the middleware")
-//     store.dispatch()
-// }
