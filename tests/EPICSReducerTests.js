c54ca084464fdc1d39cc7fff4754b7b461dd452a
Benedict Wagnall <benedict.wagnall@diamond.ac.uk>
2018-04-05 16:57:04 +0100

    Fixed second test for reducer

--- tests/EPICSReducerTests.js
+++ tests/EPICSReducerTests.js
@@ -1,6 +1,7 @@
 import * as enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-enzyme.configure({ adapter: new Adapter() });
+
+enzyme.configure({adapter: new Adapter()});
 import EPICSReducer from '../client/redux/EPICSReducer';
 import {
     UPDATE_PV
@@ -10,25 +11,26 @@ import {expect} from 'chai';
 
 describe('EPICSReducer', () => {
 
-    it('can set and return an initial state', ()=> {
+    it('can set and return an initial state', () => {
         expect(EPICSReducer(undefined, {})).to.deep.equal(
             {epicsData: {}, wsReadyState: null}
         );
     });
 
-    it('can update the state with an input', ()=> {
-        const state = {
-            EPICSValue: 0,
-            PV: null
+    it('can update the state with an input', () => {
+        const action = {
+            type: UPDATE_PV,
+            payload: {
+                pvName: 'testPVName',
+                pvValue: 123456
+            }
         };
-        expect(
-            EPICSReducer(state, {
-                type: UPDATE_PV,
-                payload: {
-                    pvName: 'thisPV',
-                    pvValue: 123456
-                }
-            })
-        ).to.deep.equal({EPICSValue: 123456, PV: 'thisPV'});
+        expect(EPICSReducer([], action)).to.deep.equal({
+            epicsData: {
+                testPVName: 123456
+            }
+
+        });
     });
+
 });
