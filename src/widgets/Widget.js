7bd372f11a182e4bdd572a098fd7de7d8cb03e83
Will Rogers <wahrogers@gmail.com>
2019-04-19 07:18:50 +0100

    Make absolute layout configurable.

--- src/widgets/Widget.js
+++ src/widgets/Widget.js
@@ -4,25 +4,34 @@ import PropTypes from 'prop-types';
 import assign from 'lodash/assign';
 
 
-export function widgetHoc(Widget, defaultStyle) {
+export const ABSOLUTE_LAYOUT = 'absolute';
+export const RELATIVE_LAYOUT = 'relative';
+
+
+export function widgetHoc(Widget, defaultStyle = {}, layout = ABSOLUTE_LAYOUT) {
 
     class WrappedWidget extends React.Component {
 
         constructor(props) {
             super(props);
             this.defaultStyle = defaultStyle;
+            this.layout = layout;
             this.updateStyle = this.updateStyle.bind(this);
-            this.widgetStyle = {
-                position: 'absolute',
-                left: this.props.x + 'px',
-                top: this.props.y + 'px',
-                height: this.props.h + 'px',
-                width: this.props.w + 'px'
-            };
             this.style = {};
         }
 
         updateStyle() {
+            if (this.layout === ABSOLUTE_LAYOUT) {
+                this.widgetStyle = {
+                    position: 'absolute',
+                    left: this.props.x + 'px',
+                    top: this.props.y + 'px',
+                    height: this.props.h + 'px',
+                    width: this.props.w + 'px'
+                };
+            } else {
+                this.widgetStyle = {};
+            }
             assign(this.style, this.defaultStyle);
             assign(this.style, this.props.style);
             assign(this.style, this.widgetStyle);
