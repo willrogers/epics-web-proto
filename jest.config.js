aca77d267c812e2dbd23ce0585a4f3b88cbb2f60
Will Rogers <wahrogers@gmail.com>
2019-04-19 11:34:51 +0100

    Single quotes.

--- jest.config.js
+++ jest.config.js
@@ -143,8 +143,8 @@ module.exports = {
 
     // The glob patterns Jest uses to detect test files
     testMatch: [
-        "**/tests/**/*.[jt]s?(x)",
-        "**/?(*.)+(spec|test).[tj]s?(x)"
+        '**/tests/**/*.[jt]s?(x)',
+        '**/?(*.)+(spec|test).[tj]s?(x)'
     ],
 
     // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
